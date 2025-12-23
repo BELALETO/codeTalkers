7968132603496df556c96d867e444bb3d5d59023
belal sryo <belalsryo@gmail.com>
2025-12-23 22:44:20 +0200

    feat: create BullMQ worker for the submission queue

---
+++ src/workers/submissionWorker.js
@@ -0,0 +1,26 @@
+// const submissionQueue = require('../queues/submissionQueue');
+const { Worker } = require('bullmq');
+
+const submissionWorker = new Worker(
+  'submissionQueue',
+  async (job) => {
+    console.log('Job started:', job.id);
+    console.log(job.data);
+    return job.data;
+  },
+  {
+    connection: {
+      host: 'localhost',
+      port: 6379
+    }
+  }
+);
+
+submissionWorker.on('completed', (job) => {
+  console.log('Job completed:', job.id);
+});
+submissionWorker.on('failed', (job) => {
+  console.log('Job failed:', job.id);
+});
+
+module.exports = submissionWorker;
