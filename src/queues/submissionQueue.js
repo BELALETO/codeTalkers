7ab00d9ebdb1d8c4ac5e54f65e95563a1f1702bd
belal sryo <belalsryo@gmail.com>
2025-12-23 22:23:21 +0200

    feat: Introduce BullMQ submission queue.

---
+++ src/queues/submissionQueue.js
@@ -0,0 +1,11 @@
+const { Queue } = require('bullmq');
+
+const submissionQueue = new Queue('submissionQueue', {
+    connection: {
+        host: 'localhost',
+        port: 6379
+    }
+});
+
+
+module.exports = submissionQueue;
